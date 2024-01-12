07dcdc2f7674ed39addb27d3eb1e62d096abddfe
unknown <fabian.granero@gmail.com>
2024-01-12 19:11:02 -0300

    Cambio de estructura de proyecto

---
+++ GestionCliente.DAL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace GestionCliente.DAL
+{
+    public class Class1
+    {
+
+    }
+}
--- GestionCliente.WebApi/obj/Debug/net8.0/GestionClienteAPI.AssemblyInfo.cs
+++ GestionCliente.WebApi/obj/Debug/net8.0/GestionClienteAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionClienteAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860e30e1f3fbeca12ab392ea8bfe4d593ffd8463")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionClienteAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionClienteAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
